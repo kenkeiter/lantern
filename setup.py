eb365394dfcd6c4e579b3654abe4be6ef3b64209
kenkeiter <ken@kenkeiter.com>
2017-02-11 00:15:39 -0500

    Fix version incompatibility issue with setup.py

--- setup.py
+++ setup.py
@@ -12,28 +12,21 @@ except ImportError:
 
 def read_requirements():
     '''parses requirements from requirements.txt'''
-    reqs_path = os.path.join(__location__, 'requirements.txt')
+    reqs_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
     install_reqs = parse_requirements(reqs_path, session=PipSession())
     reqs = [str(ir.req) for ir in install_reqs]
     return reqs
 
-def readme_handler(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as fh:
-        readme = fh.read()
-    return readme
-
 if __name__ == '__main__':
-      required = [str(ir.req) for ir in parse_requirements("requirements.txt")]
-
-setup(name='yn-lantern',
-      version='1.0.0',
-      description='A library for communicating with Yongnuo LED video lights.',
-      url='https://github.com/kenkeiter/lantern',
-      author='Ken Keiter',
-      author_email='ken@kenkeiter.com',
-      license='MIT',
-      packages=['lantern'],
-      include_package_data=True,
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      install_requires=required)
+    setup(name='yn-lantern',
+          version='1.0.0',
+          description='A library for communicating with Yongnuo LED video lights.',
+          url='https://github.com/kenkeiter/lantern',
+          author='Ken Keiter',
+          author_email='ken@kenkeiter.com',
+          license='MIT',
+          packages=['lantern'],
+          include_package_data=True,
+          test_suite='nose.collector',
+          tests_require=['nose'],
+          install_requires=read_requirements())
